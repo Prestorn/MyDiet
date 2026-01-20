0d4177aa9489efbf6a8fbe193c761a3f3ff26ccb
Alexander Yurkov <yr@example.com>
2026-01-20 15:26:43 +0300

    Удалены лишние импорты и отладочная информация

--- app/src/main/java/com/example/mydiet/presentation/ui/BaseListItem.kt
+++ app/src/main/java/com/example/mydiet/presentation/ui/BaseListItem.kt
@@ -1,14 +1,11 @@
 package com.example.mydiet.presentation.ui
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -34,9 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
