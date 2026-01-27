bf3f7b7df9a66888a447e7328179dbd2def5e561
Alexander Yurkov <yr@example.com>
2026-01-28 02:05:27 +0300

    Добавлен тестовый вызов экрана диет в MainActivity

--- app/src/main/java/com/example/mydiet/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mydiet/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mydiet.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.mydiet.presentation.ui.dietslist.DietsListScreenControl
 import com.example.mydiet.presentation.ui.dietslist.DietsListViewModel
 import com.example.mydiet.presentation.ui.theme.MyDietTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -16,9 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyDietTheme {
-
-            }
+            DietsListScreenControl(
+                viewModel = dietsListViewModel,
+                onListItemClick = {id -> Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show()}
+            )
         }
     }
 }
