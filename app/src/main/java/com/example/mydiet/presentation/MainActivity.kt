508dbc18372025ac7ddb4dccf9f6cfefddd5a121
Alexander Yurkov <yr@example.com>
2026-01-30 15:53:15 +0300

    Применение Scaffold перенесено в MainActivity

--- app/src/main/java/com/example/mydiet/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mydiet/presentation/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.mydiet.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.mydiet.presentation.ui.dietslist.DietsListScreenControl
 import com.example.mydiet.presentation.ui.dietslist.DietsListViewModel
+import com.example.mydiet.presentation.ui.theme.Background
 import com.example.mydiet.presentation.ui.theme.MyDietTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,8 +23,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyDietTheme {
-
+            Scaffold(
+                containerColor = Background
+            ) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    DietsListScreenControl(
+                        viewModel = dietsListViewModel,
+                        onListItemClick = { Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_SHORT).show() }
+                    )
+                }
             }
         }
     }
