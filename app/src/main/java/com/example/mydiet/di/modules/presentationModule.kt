a529082fe9830cf6c476de34ed353a00a2e1dcd1
Alexander Yurkov <yr@example.com>
2026-02-12 20:30:29 +0300

    Обновлён модуль DI для создания DietViewModel. Добавлен файл маршрутов

--- app/src/main/java/com/example/mydiet/di/modules/presentationModule.kt
+++ app/src/main/java/com/example/mydiet/di/modules/presentationModule.kt
@@ -1,9 +1,11 @@
 package com.example.mydiet.di.modules
 
+import com.example.mydiet.presentation.ui.diet.DietViewModel
 import com.example.mydiet.presentation.ui.dietslist.DietsListViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModelOf(::DietsListViewModel)
+    viewModelOf(::DietViewModel)
 }
